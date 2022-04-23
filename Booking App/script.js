0cc50f161796fa5ad29276e9da2cb70a0fe18b0e
kiransalve <salvekiran2011@gmail.com>
2022-04-23 21:41:45 +0530

    delete and edit user details

--- Booking App/script.js
+++ Booking App/script.js
@@ -25,13 +25,32 @@ function onsubmit(e) {
     localStorage.setItem(myObj.email, obj_serialized);
 
     showUser(myObj);
-    // let deserialized = JSON.parse(localStorage.getItem("myObj"));
-    // console.log(deserialized);
   }
-  function showUser(user) {
-    const parentNode = document.getElementById("users");
-    const childElm = `<li> ${user.name} - ${user.email}`;
-    parentNode.innerHTML = parentNode.innerHTML + childElm;
-    
+}
+// show user
+function showUser(user) {
+  let details = localStorage.getItem(user.email);
+  if (details !== null) {
+    removeItems(user.email);
+  }
+  // get parent node
+  const parentNode = document.getElementById("users");
+  // append li with new user details and delete button
+  const childElm = `<li id=${user.email}> ${user.name} - ${user.email}
+    <button onclick = deleteUser('${user.email}')> Delete </button>
+    <button onclick = deleteUser('${user.email}')> Edit </button></li>`;
+  parentNode.innerHTML = parentNode.innerHTML + childElm;
+}
+
+function deleteUser(email) {
+  localStorage.removeItem(email);
+  removeItems(email);
+}
+
+function removeItems(email) {
+  const parentNode = document.getElementById("users");
+  const childNodeToDelete = document.getElementById(email);
+  if (childNodeToDelete) {
+    parentNode.removeChild(childNodeToDelete);
   }
 }
