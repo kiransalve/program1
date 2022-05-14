5ddcb8b342e38fe696f453359efd031ab9b2c4ba
kiransalve <salvekiran2011@gmail.com>
2022-05-14 17:33:54 +0530

    Exp track 2

--- expenseTrackers/script.js
+++ expenseTrackers/script.js
@@ -15,10 +15,37 @@ function addExpense(e) {
     description: desc.value,
     category: category.value,
   };
-  //console.log(expense.amount);
-  localStorage.setItem("amt", expense.amount);
-  showExpense()
 
+  // console.log(expense.amount);
+  let obj_serialized = JSON.stringify(expense);
+
+  localStorage.setItem(expense.amount, obj_serialized);
+  showExpense(expense);
 }
 
+function showExpense(expense) {
+  let details = localStorage.getItem(expense.amount);
+  if (details !== null) {
+    removeItems(expense.amount);
+  }
+  // get parent node
+  const parentNode = document.getElementById("exDetails");
 
+  // append li with new user details and delete button
+  const childElm = `<li id=${expense.description}> ${expense.amount} - ${expense.category}
+    <button onclick = deleteUser('${expense.amount}')> Delete </button>
+    <button onclick = deleteUser('${expense.amount}')> Edit </button></li>`;
+  parentNode.innerHTML = parentNode.innerHTML + childElm;
+}
+function deleteUser(amount) {
+  localStorage.removeItem(amount);
+  removeItems(amount);
+}
+
+function removeItems(amount) {
+  const parentNode = document.getElementById("exDetails");
+  const childNodeToDelete = document.getElementById(amount);
+  if (childNodeToDelete) {
+    parentNode.removeChild(childNodeToDelete);
+  }
+}
